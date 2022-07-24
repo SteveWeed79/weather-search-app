80351a8e13f2659f15e6a79183ea410487a3df8d
unknown <steveweed1979@gmail.com>
2022-07-23 23:55:01 -0500

    Current Weather is now correct.  Move on to forecast and uv index.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,14 @@
-var lat = ('');
-var long = ('');
+var latEl;
+var longEl;
 var cityName = ('');
 var apiKey = ('77994328a557e9c9acde8c1f22078d7d');
 var cityEl = document.getElementById('citySpan');
-var locationEl = {}
+var locationEl = [];
+var currentWeather = [];
+var currentTemp = ('');
+var currentWind = ('');
+var currentHumid = ('');
+var currentUV = ('');
 
 
 
@@ -21,23 +26,46 @@ $(document).ready(function () {
 
 
 function getCityLocation() {
-    fetch('https://api.openweathermap.org/geo/1.0/direct?appid=77994328a557e9c9acde8c1f22078d7d&q=' + cityName + ',US')
+    fetch('https://api.openweathermap.org/geo/1.0/direct?appid=' + apiKey + '&q=' + cityName + ',US')
         .then(response => response.json())
-        .then(data => console.log(data))
-    locationEl = data;
-    lat = locationEl.lat
-    long = locationEl.lon
-    getWeather()
+        .then(data => {
+            locationEl = data;
+            latEl = locationEl[0].lat
+            longEl = locationEl[0].lon
+            console.log(locationEl)
+            console.log(latEl)
+            console.log(longEl)
+            getWeather()
+        })
 }
 
 
 function getWeather() {
 
-    fetch('https://api.openweathermap.org/data/2.5/weather?appid=77994328a557e9c9acde8c1f22078d7d&lat=' + lat + '&lon=' + long + '&exclude=hourly,daily')
+    fetch('https://api.openweathermap.org/data/2.5/weather?appid=' + apiKey + '&lat=' + latEl + '&lon=' + longEl + '&exclude=hourly,daily&units=imperial')
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(data => {
+            console.log(data)
+            currentWeather = data;
+            console.log(currentWeather)
+            currentTemp = currentWeather.main.temp
+            currentWind = currentWeather.wind.speed
+            currentHumid = currentWeather.main.humidity
+            //currentUV = currentWeather
+            console.log(currentTemp)
+            printData()
+            return
+        })
+
+
 }
 
-
+function printData() {
+    console.log(currentTemp)
+    document.getElementById('currTemp').innerHTML = "Temperature: " + currentTemp;
+    document.getElementById('currWind').innerHTML = "Wind Speed: " + currentWind + " mph";
+    document.getElementById('currHumidity').innerHTML = "Humidity: " + currentHumid + "%";
+    //document.getElementById('currUV').innerHTML = "UV index: " + currentUV;
+}
 
 
