dd3f70e0bea37f71870883b87e21be25c663f225
unknown <steveweed1979@gmail.com>
2022-08-13 13:37:15 -0500

    Working in dev to clean up code and learn map and foreach

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,7 +8,7 @@ var cityStorage = JSON.parse(localStorage.getItem("pastcities")) || [];
 var locationEl = [];
 var currentWeather = [];
 var currentTemp;
-var currentWind ;
+var currentWind;
 var currentHumid;
 var currentUV;
 var cityNameEL;
@@ -49,11 +49,11 @@ var dayFiveHum;
 
 //Creates the button based on the input form input
 function createButton() {
-for (let i = 0; i < cityStorage.length; i++) {
-    const city = cityStorage[i];
-    var button = $("<button>").text(city)
-    $("#listGroup").append(button)
-}
+    for (let i = 0; i < cityStorage.length; i++) {
+        const city = cityStorage[i];
+        var button = $("<button>").text(city)
+        $("#listGroup").append(button)
+    }
 
 }
 
@@ -61,13 +61,13 @@ for (let i = 0; i < cityStorage.length; i++) {
 
 
 $(document).ready(function () {
-//button click event to grab the user input and start the data collection process
+    //button click event to grab the user input and start the data collection process
     $("button").on("click", function () {
         var value = $(this)
             .siblings('.form-control')
             .val()
 
-        console.log(value)
+
         cityName = value
         getCityLocation(cityName)
         cityStorage.push(value)
@@ -85,8 +85,8 @@ $("#listGroup").on("click", "button", getCityLocation)
 
 //Pulls the lat and long based on a city name entered into the input
 function getCityLocation() {
-    if($(this).parent().attr("id") === "listGroup") {
-        cityName=$(this).text()
+    if ($(this).parent().attr("id") === "listGroup") {
+        cityName = $(this).text()
     }
     fetch('https://api.openweathermap.org/geo/1.0/direct?appid=' + apiKey + '&q=' + cityName + ',US')
         .then(response => response.json())
@@ -102,50 +102,53 @@ function getCityLocation() {
 
 
 //Pulls the weather API and information for the current weather and 5 day forcast
-function getWeather() {
-
-    fetch('https://api.openweathermap.org/data/2.5/onecall?appid=' + apiKey + '&lat=' + latEl + '&lon=' + longEl + '&exclude=hourly,minutely&units=imperial')
-        .then(response => response.json())
-        .then(data => {
-            currentWeather = data;
-            console.log(currentWeather)
-            currentTemp = currentWeather.current.temp
-            currentWind = currentWeather.current.wind_speed
-            currentHumid = currentWeather.current.humidity
-            currentUV = currentWeather.current.uvi
-            currentCloudIcon = currentWeather.current.weather[0].icon
-
-            dayOneIcon = currentWeather.daily[0].weather[0].icon
-            dayOneTemp = currentWeather.daily[0].temp.day
-            dayOneWind = currentWeather.daily[0].wind_speed
-            dayOneHum = currentWeather.daily[0].humidity
-
-            dayTwoIcon = currentWeather.daily[1].weather[0].icon
-            dayTwoTemp = currentWeather.daily[1].temp.day
-            dayTwoWind = currentWeather.daily[1].wind_speed
-            dayTwoHum = currentWeather.daily[1].humidity
-
-            dayThreeIcon = currentWeather.daily[2].weather[0].icon
-            dayThreeTemp = currentWeather.daily[2].temp.day
-            dayThreeWind = currentWeather.daily[2].wind_speed
-            dayThreeHum = currentWeather.daily[2].humidity
-
-            dayFourIcon = currentWeather.daily[3].weather[0].icon
-            dayFourTemp = currentWeather.daily[3].temp.day
-            dayFourWind = currentWeather.daily[3].wind_speed
-            dayFourHum = currentWeather.daily[3].humidity
-
-            dayFiveIcon = currentWeather.daily[4].weather[0].icon
-            dayFiveTemp = currentWeather.daily[4].temp.day
-            dayFiveWind = currentWeather.daily[4].wind_speed
-            dayFiveHum = currentWeather.daily[4].humidity
-
-            printData()
-            return
-        })
+async function getWeather() {
+    const weatherSearch = 'https://api.openweathermap.org/data/2.5/onecall?appid=' + apiKey + '&lat=' + latEl + '&lon=' + longEl + '&exclude=hourly,minutely&units=imperial';
+    const response = await fetch(weatherSearch);
+    const body = await response.json();
+    currentWeather = body;
+    console.log(currentWeather)
+    body.daily.map(async (body) => {
 
 
-}
+        let cardInfo = (`
+        
+            <div class="card-body">
+                <h5 class="card-title" id="day1Date">Day 1</h5>
+                <img src="${currentWeather.daily[0].weather[0].icon}" id="day1Icon" alt="">
+                <br>
+                <span>Temperature: ${currentWeather.daily[0].temp.day}</span>
+                <br>
+                <span>Wind Speed: ${currentWeather.daily[0].wind_speed}</span>
+                <br>
+                <span>Humidity: ${currentWeather.daily[0].humidity}</span>
+                <br>
+            </div>
+        
+
+        `);
+
+        document.getElementById('cardDeck').innerHTML = cardInfo;
+
+        console.log(body)
+
+        currentTemp = currentWeather.current.temp
+        console.log(currentTemp)
+        currentWind = currentWeather.current.wind_speed
+        currentHumid = currentWeather.current.humidity
+        currentUV = currentWeather.current.uvi
+        currentCloudIcon = currentWeather.current.weather[0].icon
+
+
+
+
+        printData()
+
+
+    })
+
+
+};
 
 
 // This prints all the pulled data to the screen for the user to read.
@@ -158,53 +161,39 @@ function printData() {
     document.getElementById("currentIcon").src = "http://openweathermap.org/img/wn/" + currentCloudIcon + "@2x.png";
 
 
-    document.getElementById("day1Icon").src = "http://openweathermap.org/img/wn/" + dayOneIcon + "@2x.png";
-    document.getElementById('day1Temp').innerHTML = 'Temperature: ' + dayOneTemp + "° F";
-    document.getElementById('day1Wind').innerHTML = 'Wind Speed ' + dayOneWind + ' mph';
-    document.getElementById('day1Hum').innerHTML = 'Humidity: ' + dayOneHum + '%';
 
 
-    document.getElementById("day2Icon").src = "http://openweathermap.org/img/wn/" + dayTwoIcon + "@2x.png";
-    document.getElementById('day2Temp').innerHTML = 'Temperature: ' + dayTwoTemp + "° F";
-    document.getElementById('day2Wind').innerHTML = 'Wind Speed ' + dayTwoWind + ' mph';
-    document.getElementById('day2Hum').innerHTML = 'Humidity: ' + dayTwoHum + '%';
+    // document.getElementById("day1Icon").src = "http://openweathermap.org/img/wn/" + dayOneIcon + "@2x.png";
+    // document.getElementById('day1Temp').innerHTML = 'Temperature: ' + dayOneTemp + "° F";
+    // document.getElementById('day1Wind').innerHTML = 'Wind Speed ' + dayOneWind + ' mph';
+    // document.getElementById('day1Hum').innerHTML = 'Humidity: ' + dayOneHum + '%';
 
 
-    document.getElementById("day3Icon").src = "http://openweathermap.org/img/wn/" + dayThreeIcon + "@2x.png";
-    document.getElementById('day3Temp').innerHTML = 'Temperature: ' + dayThreeTemp + "° F";
-    document.getElementById('day3Wind').innerHTML = 'Wind Speed ' + dayThreeWind + ' mph';
-    document.getElementById('day3Hum').innerHTML = 'Humidity: ' + dayThreeHum + '%';
+    // document.getElementById("day2Icon").src = "http://openweathermap.org/img/wn/" + dayTwoIcon + "@2x.png";
+    // document.getElementById('day2Temp').innerHTML = 'Temperature: ' + dayTwoTemp + "° F";
+    // document.getElementById('day2Wind').innerHTML = 'Wind Speed ' + dayTwoWind + ' mph';
+    // document.getElementById('day2Hum').innerHTML = 'Humidity: ' + dayTwoHum + '%';
 
 
-    document.getElementById("day4Icon").src = "http://openweathermap.org/img/wn/" + dayFourIcon + "@2x.png";
-    document.getElementById('day4Temp').innerHTML = 'Temperature: ' + dayFourTemp + "° F";
-    document.getElementById('day4Wind').innerHTML = 'Wind Speed ' + dayFourWind + ' mph';
-    document.getElementById('day4Hum').innerHTML = 'Humidity: ' + dayFourHum + '%';
+    // document.getElementById("day3Icon").src = "http://openweathermap.org/img/wn/" + dayThreeIcon + "@2x.png";
+    // document.getElementById('day3Temp').innerHTML = 'Temperature: ' + dayThreeTemp + "° F";
+    // document.getElementById('day3Wind').innerHTML = 'Wind Speed ' + dayThreeWind + ' mph';
+    // document.getElementById('day3Hum').innerHTML = 'Humidity: ' + dayThreeHum + '%';
 
 
-    document.getElementById("day5Icon").src = "http://openweathermap.org/img/wn/" + dayFiveIcon + "@2x.png";
-    document.getElementById('day5Temp').innerHTML = 'Temperature: ' + dayFiveTemp + "° F";
-    document.getElementById('day5Wind').innerHTML = 'Wind Speed ' + dayFiveWind + ' mph';
-    document.getElementById('day5Hum').innerHTML = 'Humidity: ' + dayFiveHum + '%';
+    // document.getElementById("day4Icon").src = "http://openweathermap.org/img/wn/" + dayFourIcon + "@2x.png";
+    // document.getElementById('day4Temp').innerHTML = 'Temperature: ' + dayFourTemp + "° F";
+    // document.getElementById('day4Wind').innerHTML = 'Wind Speed ' + dayFourWind + ' mph';
+    // document.getElementById('day4Hum').innerHTML = 'Humidity: ' + dayFourHum + '%';
+
+
+    // document.getElementById("day5Icon").src = "http://openweathermap.org/img/wn/" + dayFiveIcon + "@2x.png";
+    // document.getElementById('day5Temp').innerHTML = 'Temperature: ' + dayFiveTemp + "° F";
+    // document.getElementById('day5Wind').innerHTML = 'Wind Speed ' + dayFiveWind + ' mph';
+    // document.getElementById('day5Hum').innerHTML = 'Humidity: ' + dayFiveHum + '%';
 
 
 
-
-    //Adds the date to the 5 day forecast cards.
-    var printDay1Date = moment(today, "dd/mm/yyyy").add(1, "days").format("MM/DD/YYYY")
-    document.getElementById('day1Date').innerHTML = printDay1Date;
-
-    var printDay2Date = moment(today, "dd/mm/yyyy").add(2, "days").format("MM/DD/YYYY")
-    document.getElementById('day2Date').innerHTML = printDay2Date;
-
-    var printDay3Date = moment(today, "dd/mm/yyyy").add(3, "days").format("MM/DD/YYYY")
-    document.getElementById('day3Date').innerHTML = printDay3Date;
-
-    var printDay4Date = moment(today, "dd/mm/yyyy").add(4, "days").format("MM/DD/YYYY")
-    document.getElementById('day4Date').innerHTML = printDay4Date;
-
-    var printDay5Date = moment(today, "dd/mm/yyyy").add(5, "days").format("MM/DD/YYYY")
-    document.getElementById('day5Date').innerHTML = printDay5Date;
 
     if (currentUV <= 3) {
         document.getElementById('currUV').style.color = 'white';
